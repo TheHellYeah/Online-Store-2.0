54737789e52f047ca66172ba96bcbc73b0c1d3a2
Артём <kostertem@yadnex.ru>
2020-05-25 00:53:50 +0400

    Понастроил связей, сейчас разбираться буду, хотя бы приложение запустилось

--- src/main/java/kostuchenkov/rgr/controller/IndexController.java
+++ src/main/java/kostuchenkov/rgr/controller/IndexController.java
@@ -1,20 +1,28 @@
 package kostuchenkov.rgr.controller;
 
+
+import kostuchenkov.rgr.domain.service.Mail;
 import kostuchenkov.rgr.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
-
+    @Autowired
+    Mail mailClient;
     @Autowired
     private ProductRepository productRepository;
+
+
+
 
     @GetMapping("")
     public String indexPage(Model model) {
         model.addAttribute("shoes", productRepository);
+        //mailClient.send("mitlg@yandex.ru","Ваш код активации","Привет, твой код активации 124234");
         return "index";
     }
 }
