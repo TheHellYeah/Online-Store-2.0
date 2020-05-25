2baa5f5305b988783368836530d8b2e4c8305254
Михаил <mitlg7@gmail.com>
2020-05-25 21:11:50 +0400

    Добавил шаблон и контролеер корзины

---
+++ src/main/java/kostuchenkov/rgr/controller/CartController.java
@@ -0,0 +1,22 @@
+package kostuchenkov.rgr.controller;
+
+
+import kostuchenkov.rgr.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CartController {
+    @Autowired
+    OrderRepository orderRepository;
+
+    @GetMapping("/cart")
+    public String showCart(Model model){
+
+
+
+        return "cart";
+    }
+}
