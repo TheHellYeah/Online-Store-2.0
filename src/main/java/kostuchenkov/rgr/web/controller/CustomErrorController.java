a192b57311f10d5a73310f4f904c1403e186923c
Артём <kostertem@yadnex.ru>
2020-06-05 10:17:37 +0400

    Добавил кастомную страничку ошибки

---
+++ src/main/java/kostuchenkov/rgr/web/controller/CustomErrorController.java
@@ -0,0 +1,37 @@
+package kostuchenkov.rgr.web.controller;
+
+import lombok.val;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String errorPage(HttpServletRequest request, Model model) {
+
+        val status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        if(status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+            //TODO я добавил только два статус кода пока что, не помню какие там еще у нас могут вылететь, если что по аналогии добавишь сам
+            if(statusCode == HttpStatus.NOT_FOUND.value()) {
+                model.addAttribute("error", "Запрашиваемая вами страница не найдена!");
+            }
+            if(statusCode == HttpStatus.FORBIDDEN.value()) {
+                model.addAttribute("error", "У вас недостаточно прав для просмотора запрашиваемой страницы!");
+            }
+        }
+        return "error";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+}
